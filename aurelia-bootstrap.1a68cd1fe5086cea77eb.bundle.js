e59e65fa0d2340cccf7e9ea1110a0cee8102900e
Andi Budi <andibateeq@gmail.com>
2020-11-26 15:00:56 +0700

    commit

--- aurelia-bootstrap.1a68cd1fe5086cea77eb.bundle.js
+++ aurelia-bootstrap.1a68cd1fe5086cea77eb.bundle.js
@@ -79,7 +79,7 @@
 /******/ 		script.async = true;
 /******/ 		script.timeout = 120000;
 /******/
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + {"0":"e49fca69d9206aa57e94","1":"e74b956ddd279c7e248a"}[chunkId] + ".chunk.js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + {"0":"403c8d813eb481c34ffa","1":"e74b956ddd279c7e248a"}[chunkId] + ".chunk.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
