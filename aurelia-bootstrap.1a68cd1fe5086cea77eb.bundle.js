a69e4f59f6cd8084f68af75eff823222a2795428
Andi Budi <andibateeq@gmail.com>
2020-12-14 10:43:51 +0700

    update
    14/12/2020

--- aurelia-bootstrap.1a68cd1fe5086cea77eb.bundle.js
+++ aurelia-bootstrap.1a68cd1fe5086cea77eb.bundle.js
@@ -79,7 +79,7 @@
 /******/ 		script.async = true;
 /******/ 		script.timeout = 120000;
 /******/
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + {"0":"403c8d813eb481c34ffa","1":"e74b956ddd279c7e248a"}[chunkId] + ".chunk.js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + {"0":"bf278d28c54f0ee014e2","1":"e74b956ddd279c7e248a"}[chunkId] + ".chunk.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
